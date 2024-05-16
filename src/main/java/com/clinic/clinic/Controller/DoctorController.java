2e1e0db4440273d2641038fcc7f2db161107e7ad
MykhailoLytvyn <cresedel211194@gmail.com>
2024-05-16 12:59:02 +0200

    diagrams

--- src/main/java/com/clinic/clinic/Controller/DoctorController.java
+++ src/main/java/com/clinic/clinic/Controller/DoctorController.java
@@ -5,7 +5,6 @@ import com.clinic.clinic.Entity.User;
 import com.clinic.clinic.Service.DoctorService;
 import com.clinic.clinic.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
